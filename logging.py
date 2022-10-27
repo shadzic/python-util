9405bbba0551e83c7619f65d6f120d0a0113ddd7
Selma <shadzic@users.noreply.github.com>
2022-10-27 15:39:01 +0200

    Update logging.py

--- logging.py
+++ logging.py
@@ -6,3 +6,17 @@ logger.addHandler(console_handler)
 logger.setLevel(logging.DEBUG)
 
 logger.info("This is a very informative message, have a great day!")
+
+
+# we can put this in a function and then import in the rest of the files:
+def get_logger(name):
+
+	logger = logging.getLogger(name)
+	console_handler = logging.StreamHandler()
+	logger.addHandler(console_handler)
+	logger.setLevel(logging.DEBUG)
+
+	return logger
+
+# in the other files: 
+logger = get_logger(__name__)
